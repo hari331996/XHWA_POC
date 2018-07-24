fd80cd79d6630157167d2b73fb9dc00e4f24da05
Pooja Jain <pooja.jain@einfochips.com>
2018-07-24 10:26:51 +0530

    Added pod files login API call and eventsByDay API call

--- Login Screen/Controller/ActivityTabViewController.swift
+++ Login Screen/Controller/ActivityTabViewController.swift
@@ -7,20 +7,80 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ActivityTabViewController: UIViewController {
-
+class ActivityTabViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let apiUrl = "https://qacluster6.einfochips.com/rest/icontrol/sites/4140/eventsByDay"
+    
+    // cell reuse id (cells that scroll out of view can be reused)
+    let cellReuseIdentifier = "cell"
+    
+    // don't forget to hook this up from the storyboard
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        print("Activity Tab View Controller Loaded")
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+        let params : [String : String] = ["startDate" : "2018-07-22", "maxResults" : "65536"]
+        getActivityData(url: apiUrl, parameters: params)
+    }
+    
+    func getActivityData(url: String, parameters: [String: String]) {
+        let headers = ["Accept": "application/json; charset=UTF-8"]
+        Alamofire.request(url, method: .get, parameters: parameters, headers: headers)
+        .responseString {
+            response in
+            if response.result.isSuccess {
+                
+                let activityJSON : JSON = JSON(response.result.value!)
+                self.updateUI(json: activityJSON)
+                
+            }
+            else {
+                print("Error \(String(describing: response.result.error))")
+            }
+        }
+    }
+    
+    func updateUI(json: JSON) {
+//        json["weather"][0]["id"].intValue
+        print("updateUI")
+        print(json)
+        print(json["entry"].arrayValue)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return self.animals.count
+        return 1
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
+        
+        // set the text from the data model
+        //cell.textLabel?.text = self.animals[indexPath.row]
+        
+        return cell
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
+    
     
 
     /*
--- Login Screen/ViewController.swift
+++ Login Screen/ViewController.swift
@@ -7,31 +7,56 @@
 //
 
 import UIKit
-import CoreData
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController, UITextFieldDelegate {
     var commitPredicate: NSPredicate?
+    let apiUrl = "https://qacluster6.einfochips.com/rest/icontrol/login"
+    @IBOutlet weak var usernameView: UITextField!
+    
+    @IBOutlet weak var passwordView: UITextField!
+    
+    @IBOutlet weak var resultTxt: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //Save Data to Core Data Database
-        print("Login Controller Loaded")
-        
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        
-        let context = appDelegate.persistentContainer.viewContext
-        
-        let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
-        
-        newUser.setValue(3, forKey: "id")
-        newUser.setValue("einfo", forKey: "name")
-        newUser.setValue("1234", forKey: "password")
-        
-        do {
-            try context.save()
-            print("Data Saved Successfully")
-        } catch {
-            print("Failed to save data")
+    }
+    
+    func doLogin(url: String) {
+        let headers = ["X-login": String(usernameView.text!),
+                       "X-password": String(passwordView.text!),
+                       "X-AppKey": "defaultKey",
+                       "X-expires": "6000000",
+                       "Accept": "application/json; charset=UTF-8"]
+        Alamofire.request(url, method: .get, headers: headers)
+            .responseString {
+                response in
+
+                    if response.result.isSuccess {
+
+                    let loginJSON : JSON = JSON(response.result.value!)
+                    if (loginJSON != "failed") {
+                        print(loginJSON)
+                        let uname = loginJSON["login"]["displayName"].stringValue
+                        print(uname)
+                        //to navigate to next page
+                        self.performSegue(withIdentifier: "segueIdMain", sender: self)
+                    }
+                    else {
+                        print("Login Failed!!")
+                        self.resultTxt.alpha = 1
+                        self.resultTxt.text = "Oops..Log In Failed!!"
+                        //Alert View
+                        let alert = UIAlertController(title: "Alert", message: "Login Failed..Invalid Credentials", preferredStyle: UIAlertControllerStyle.alert)
+                        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+
+                }
+                else {
+                    print("Error \(String(describing: response.result.error))")
+                }
         }
     }
 
@@ -39,49 +64,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBOutlet weak var usernameView: UITextField!
-    
-    @IBOutlet weak var passwordView: UITextField!
-    
-    @IBOutlet weak var resultTxt: UILabel!
     
     @IBAction func signInBtnTapped(_ sender: UIButton) {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        
-        let context = appDelegate.persistentContainer.viewContext
-        
-        let request = NSFetchRequest<NSFetchRequestResult> (entityName: "Users")
-        request.predicate = commitPredicate
-//        request.predicate = NSPredicate(format: "username < %@", "Heena")
-        request.returnsObjectsAsFaults = false
-        
-        do {
-            let results = try context.fetch(request)
-            
-            for result in results as! [NSManagedObject] {
-                if let username = result.value(forKey: "name") as? String, let password = result.value(forKey: "password") as? String {
-                    if username == usernameView.text!, password == passwordView.text! {
-                        print("Login Successful")
-                        
-                        //to navigate to next page
-                        self.performSegue(withIdentifier: "segueIdMain", sender: self)
-                    }
-                    else {
-                        print("Login Failed!!")
-                        resultTxt.alpha = 1
-                        resultTxt.text = "Oops..Log In Failed!!"
-                        
-                        //Alert View
-                        let alert = UIAlertController(title: "Alert", message: "Login Failed..Invalid Credentials", preferredStyle: UIAlertControllerStyle.alert)
-                        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
-                        self.present(alert, animated: true, completion: nil)
-                    }
-                }
-            }
-        } catch {
-            
-        }
+        doLogin(url: apiUrl)
     }
     
     @IBAction func helpBtnTapped(_ sender: UIButton) {
